4c03337858ec1f1bbd2c49eeb9d8b765c2a90acc
dagpan <panagiotisntagkinis@gmail.com>
2020-02-24 16:34:10 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: 'AHA', last_name: 'BAH')
+Student.create(first_name: 'OHA', last_name: 'JAH')
+Student.create(first_name: 'AHO', last_name: 'HUH')
+Student.create(first_name: 'OHO', last_name: 'UUH')
+Student.create(first_name: 'IHA', last_name: 'HUB')
+Student.create(first_name: 'AHI', last_name: 'XAH')
+Student.create(first_name: 'IHI', last_name: 'ZAH')
+Student.create(first_name: 'EHA', last_name: 'BAL')
+Student.create(first_name: 'AHE', last_name: 'DAH')
+Student.create(first_name: 'EHE', last_name: 'GAH')
+Student.create(first_name: 'UHA', last_name: 'LAH')
+Student.create(first_name: 'AHU', last_name: 'DAH')
+Student.create(first_name: 'UHU', last_name: 'LAB')
+
